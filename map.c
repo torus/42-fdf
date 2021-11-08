26093af5675d83a0a444ed91698a3a56a2575d91
Toru Hisai <toru@torus.jp>
2021-11-08 20:22:05 +0900

    cleanup

--- map.c
+++ map.c
@@ -18,10 +18,6 @@
 
 void	c3_map_init(t_c3_map *map, t_c3_scene *scene)
 {
-	/* int		i; */
-	/* char	ch; */
-
-	/* map->map = scene->map; */
 	map->width = scene->map_width;
 	map->height = scene->map_height;
 	printf("init width height: %d %d\n", map->width, map->height);
@@ -32,7 +28,6 @@ void	c3_map_init(t_c3_map *map, t_c3_scene *scene)
 		exit(1);
 	}
 	ft_bzero(map->map, sizeof(int) * map->width * map->height);
-	/* i = 0; */
 
 	t_c3_map_rows	*row;
 	t_c3_map_cells	*cell;
@@ -47,74 +42,23 @@ void	c3_map_init(t_c3_map *map, t_c3_scene *scene)
 		cell = row->cells;
 		while(cell)
 		{
-			/* map->map[i * map->width + j] = cell->value; */
 			map->map[(map->height - i - 1) * map->width + map->width - j - 1] = cell->value;
-			/* printf("init %d %d %d\n", j, i, cell->value); */
 			cell = cell->next;
 			j++;
 		}
 		row = row->next;
 		i++;
 	}
-	/* while (i < map->width * map->height) */
-	/* { */
-	/* 	ch = map->map[i]; */
-	/* 	if (ch != ' ' && ch != '0' && ch != '1' && ch != '2' && */
-	/* 		ch != 'N' && ch != 'E' && ch != 'S' && ch != 'W') */
-	/* 	{ */
-	/* 		C3_LOG("Error\nInvalid map.\n"); */
-	/* 		c3_scene_cleanup(scene); */
-	/* 		exit(1); */
-	/* 	} */
-	/* 	i++; */
-	/* } */
 }
 
 #include <math.h>
 int		c3_query_map(t_c3_state *stat, int x, int y)
 {
-	/* ch = stat->map.map[y * stat->map.width + x]; */
-	/* if (ch == '1') */
-	/* 	return (1); */
-	/* else if (ch == '0') */
-	/* 	return (0); */
-	/* else if (ch == '2') */
-	/* 	return (2); */
-	/* return (0); */
-
 	int	dx;
 	int	dy;
 	int	height;
-
-	/* printf("query: %d, %d\n", x, y); */
-
-	/* dx = x - ((t_c3_scene*)stat->scene)->map_width / 2; */
-	/* dy = y - ((t_c3_scene*)stat->scene)->map_height / 2; */
-	/* height = 10 * sin((dx * dx + dy * dy) / 30.) + 10; */
 
 	height = stat->map.map[y * stat->map.width + x];
 
 	return (height);
 }
-
-/* void	c3_check_map_closed(t_c3_state *stat, int x, int y) */
-/* { */
-/* 	char	ch; */
-
-/* 	c3_check_map_closed_iter(stat, x, y); */
-/* 	y = 0; */
-/* 	while (y < stat->map.height) */
-/* 	{ */
-/* 		x = 0; */
-/* 		while (x < stat->map.width) */
-/* 		{ */
-/* 			ch = stat->map.map[y * stat->map.width + x]; */
-/* 			if (ch == 'x') */
-/* 				stat->map.map[y * stat->map.width + x] = '0'; */
-/* 			else if (ch == 'o') */
-/* 				stat->map.map[y * stat->map.width + x] = '2'; */
-/* 			x++; */
-/* 		} */
-/* 		y++; */
-/* 	} */
-/* } */
